19ba5380b2e335b3a0801e0d5e9a4c29d7aeca4f
Timotius2005 <timotiuskristafaelharjanto2005@mail.ugm.ac.id>
2024-11-26 12:32:21 +0700

    Add task post API in taskController and taskRoutes

--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { isAdminRoute, protectRoute } from '../middlewares/authMiddleware';
+import { createTask, duplicateTask, postTaskActivity } from '../controllers/taskController';
 
 const router = express.Router();
 router.post("/create", protectRoute, isAdminRoute, createTask);
