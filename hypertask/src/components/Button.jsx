ee065a4e333a19445c3e04df8d8d87fb530cd1f6
RakhelFadilaN <rakhelafadhilanastiti@mail.ugm.ac.id>
2024-11-26 19:15:55 +0700

    button?

--- hypertask/src/components/Button.jsx
+++ hypertask/src/components/Button.jsx
@@ -5,7 +5,10 @@ const Button = ({ icon, className, label, type, onClick = () => {} }) => {
   return (
     <button
       type={type || "button"}
-      className={clsx("px-3 py-2 outline-none", className)}
+      className={clsx(
+        "px-3 py-2 rounded-md font-bold text-lg cursor-pointer transition duration-200",
+        className
+      )}
       onClick={onClick}
     >
       <span>{label}</span>
